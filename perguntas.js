d1c46a9174a829f03b6110f6720ca583a4039a83
Thayson1 <thayson.santos@escola.pr.gov.br>
2025-06-18 14:26:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Ultrakill',
+    'Quem é Sisyphus Prime?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma rosa dos ventos?',
+    'Diagrama ultilizado para representar as direções cardeais e colaterais que auxiliam na orientação e navegação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variavel em programação?',
+    'É um espaço nomeado na memória do computador usado para armazenar valores '
+)
+
+criaCartao(
+    'História',
+    'Quando aconteceu a primeira Guerra Mundial?',
+    '28 de Julh de 1914'
+)
+
+criaCartao(
+    'Mortal Kombat',
+    'Quais o nome do Subzero Original?',
+    'Bi Han'
+)
+
+criaCartao(
+    'Hollow Knight',
+    'Como são chamados os 3 protetores do Ovo negro?',
+    'Sonhadores'
+)
